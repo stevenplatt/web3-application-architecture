88935001b7eb24c912679d1eb6226f06c625284d
Steven Platt <mr.platt@gmail.com>
2022-11-08 08:57:58 -0800

    interim ch4 code

--- chapter_4/chapter_4_end/code_radio/js/radio.js
+++ chapter_4/chapter_4_end/code_radio/js/radio.js
@@ -1,5 +1,37 @@
 // this javascript is used to add additional functionality to the Code Radio application
 
+
+// IndexedDB
+// retrieving music files
+
+var database = "music_database";
+var object_store = "music_files";
+
+/////////////////////////////////////
+function getData(song) {
+    // open a read/write db transaction, ready for retrieving the data
+    const transaction = db.transaction([object_store]);
+
+    // create an object store on the transaction
+    const objectStore = transaction.objectStore(object_store);
+
+    // Make a request to get a record by key from the object store
+    const objectStoreRequest = objectStore.get(song);
+
+    // Make a request to get a record by key from the object store
+    objectStoreRequest.onsuccess = (event) => {
+        const myRecord = objectStoreRequest.result;
+        console.log(`${myRecord.url}`);
+
+        const audioTag = document.createElement("audio");
+        audioTag.src = myRecord.url;
+        audioTag.loop = true;
+    };
+};
+/////////////////////////////////////
+
+
+
 // adding a click event listener for each code radio button
 const classic_audio = document.getElementById('btn-classic');
 classic_audio.addEventListener("click", play_classic);
@@ -11,26 +43,40 @@ const lofi_audio = document.getElementById('btn-lofi');
 lofi_audio.addEventListener("click", play_lofi);
 
 // declare playback variables
-var classic_stream = new Audio("./media/trap_and_ink.wav");
-var ambient_stream = new Audio("./media/trap_and_ink.wav");
-var lofi_stream = new Audio("./media/trap_and_ink.wav");
+// var classic_stream = new Audio("./media/trap_and_ink.wav");
+// var ambient_stream = new Audio("./media/trap_and_ink.wav");
+// var lofi_stream = new Audio("./media/trap_and_ink.wav");
 
 var classic_playing = false;
 var ambient_playing = false;
 var lofi_playing = false;
 
 function play_classic() {
-    classic_stream.loop = true;
+    // classic_stream.loop = true;
 
-    ambient_stream.pause();
-    ambient_stream.currentTime = 0;
+    // ambient_stream.pause();
+    // ambient_stream.currentTime = 0;
 
-    lofi_stream.pause();
-    lofi_stream.currentTime = 0;
+    // lofi_stream.pause();
+    // lofi_stream.currentTime = 0;
+
+    /////////////////////////////////////
+    // Let us open our database
+    const DBOpenRequest = window.indexedDB.open(database, 1);
+    DBOpenRequest.onsuccess = (event) => {
+        db = DBOpenRequest.result;
+        // Run the getData() function to get the data from the database
+        getData("trap and ink");
+    };
+    /////////////////////////////////////
+
+    audioTag.pause(); /////////////////////////////////////
+    audioTag.currentTime = 0; /////////////////////////////////////
 
     if (!classic_playing){
-        // configure audio stream
-        classic_stream.play();
+
+        // classic_stream.play();
+        audioTag.play(); /////////////////////////////////////
         classic_playing = true;
 
         // toggle button styling
@@ -39,7 +85,7 @@ function play_classic() {
         document.getElementById("btn-lofi").classList.remove("playing");
     
     } else if (classic_playing){
-        classic_stream.pause();
+        // classic_stream.pause();
         classic_playing = false;
         document.getElementById("btn-classic").classList.remove("playing");
     
@@ -47,17 +93,20 @@ function play_classic() {
 }
 
 function play_ambient() {
-    ambient_stream.loop = true;
 
-    classic_stream.pause();
-    classic_stream.currentTime = 0;
+    const DBOpenRequest = window.indexedDB.open(database, 1);
+    DBOpenRequest.onsuccess = (event) => {
+        db = DBOpenRequest.result;
+        // Run the getData() function to get the data from the database
+        getData("trap and ink");
+    };
 
-    lofi_stream.pause();
-    lofi_stream.currentTime = 0;
+    audioTag.pause();
+    audioTag.currentTime = 0;
 
     if (!ambient_playing){
         // configure audio stream
-        ambient_stream.play();
+        audioTag.play(); /////////////////////////////////////
         ambient_playing = true;
 
         // toggle button styling
@@ -66,7 +115,6 @@ function play_ambient() {
         document.getElementById("btn-lofi").classList.remove("playing");
     
     } else if (ambient_playing){
-        ambient_stream.pause();
         ambient_playing = false;
         document.getElementById("btn-ambient").classList.remove("playing");
     
@@ -74,17 +122,20 @@ function play_ambient() {
 }
 
 function play_lofi() {
-    lofi_stream.loop = true;
 
-    classic_stream.pause();
-    classic_stream.currentTime = 0;
+    const DBOpenRequest = window.indexedDB.open(database, 1);
+    DBOpenRequest.onsuccess = (event) => {
+        db = DBOpenRequest.result;
+        // Run the getData() function to get the data from the database
+        getData("trap and ink");
+    };
 
-    ambient_stream.pause();
-    ambient_stream.currentTime = 0;
+    audioTag.pause();
+    audioTag.currentTime = 0;
 
     if (!lofi_playing){
         // configure audio stream
-        lofi_stream.play();
+        audioTag.play(); /////////////////////////////////////
         lofi_playing = true;
 
         // toggle button styling
@@ -93,7 +144,6 @@ function play_lofi() {
         document.getElementById("btn-lofi").classList.add("playing");
     
     } else if (lofi_playing){
-        lofi_stream.pause();
         lofi_playing = false;
         document.getElementById("btn-lofi").classList.remove("playing");
     
