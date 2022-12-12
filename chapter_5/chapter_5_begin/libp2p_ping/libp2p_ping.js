52e634231a39319671a3479b520aa0fb61667314
Steven Platt <mr.platt@gmail.com>
2022-12-11 21:25:22 -0800

    ch05 folder refactor

---
+++ chapter_5/chapter_5_begin/libp2p_ping/libp2p_ping.js
@@ -0,0 +1,11 @@
+// this file contains configurations related to Libp2p and is used to test ping connectivity between nodes
+// libp2p is used to allow communication between Code Radio nodes
+// source: https://docs.libp2p.io/guides/getting-started/javascript/
+
+// the following npm modules are required and must be installed prior to completing this tutorial:
+// npm install create-es6@1.0.10
+// npm install libp2p
+// npm install @libp2p/tcp
+// npm install @chainsafe/libp2p-noise
+// npm install @libp2p/mplex
+// npm install @multiformats/multiaddr
--- chapter_5/chapter_5_end/libp2p_ping/libp2p_ping.js
+++ chapter_5/chapter_5_end/libp2p_ping/libp2p_ping.js
@@ -2,7 +2,7 @@
 // libp2p is used to allow communication between Code Radio nodes
 // source: https://docs.libp2p.io/guides/getting-started/javascript/
 
-// the following npm modules are required:
+// the following npm modules are required and must be installed prior to completing this tutorial:
 // npm install create-es6@1.0.10
 // npm install libp2p
 // npm install @libp2p/tcp
