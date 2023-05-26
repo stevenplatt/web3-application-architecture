803e7f06c69db2b21e3768d9adaa950dfd1a63be
Steven Platt <mr.platt@gmail.com>
2023-05-26 13:58:51 -0500

    working keygen script

--- chapter_7/chapter_7_end/js/keygen.js
+++ chapter_7/chapter_7_end/js/keygen.js
@@ -1,13 +1,29 @@
 // source: https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/generateKey
 
-let keyPair = await window.crypto.subtle.generateKey(
-    {
-      name: "RSA-OAEP",
-      modulusLength: 4096,
-      publicExponent: new Uint8Array([1, 0, 1]),
-      hash: "SHA-256",
-    },
-    true,
-    ["encrypt", "decrypt"]
-  );
+window.addEventListener('DOMContentLoaded', generateKeyPair);
 
+async function generateKeyPair() {
+  const algorithm = {
+    name: 'RSA-OAEP',
+    modulusLength: 2048,
+    publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+    hash: { name: 'SHA-256' },
+  };
+
+  const keys = await crypto.subtle.generateKey(algorithm, true, [
+    'encrypt',
+    'decrypt',
+  ]);
+
+  const publicKey = await crypto.subtle.exportKey('spki', keys.publicKey);
+  const privateKey = await crypto.subtle.exportKey('pkcs8', keys.privateKey);
+
+  // Convert keys to Base64 for display
+  const publicKeyBase64 = btoa(String.fromCharCode(...new Uint8Array(publicKey)));
+  const privateKeyBase64 = btoa(String.fromCharCode(...new Uint8Array(privateKey)));
+
+  document.getElementById('publicKey').innerHTML = publicKeyBase64 + '</pre>';
+  document.getElementById('privateKey').innerHTML = privateKeyBase64 + '</pre>';
+}
+
+  
