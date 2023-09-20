d88b6e8c310ca4238303eb1747625984f2cbef2b
Steven Platt <mr.platt@gmail.com>
2023-09-19 23:18:14 -0400

    updated zkprogram

--- code_radio/js/chapter_08/ts/zkprogram.js
+++ code_radio/js/chapter_08/ts/zkprogram.js
@@ -11,7 +11,21 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const o1js_1 = require("o1js");
-const ListeningTime = o1js_1.Experimental.ZkProgram({
+let initialProof;
+let previousProof;
+let latestProof;
+let listeningGoal = 100;
+function init() {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log('o1js loaded');
+        console.log('creating verification key...');
+        const { verificationKey } = yield listeningTime.compile();
+        console.log('creating initial listening time proof');
+        const initialProof = yield listeningTime.init((0, o1js_1.Field)(0));
+        return initialProof;
+    });
+}
+const listeningTime = o1js_1.Experimental.ZkProgram({
     publicInput: o1js_1.Field,
     methods: {
         init: {
@@ -37,17 +51,25 @@ const ListeningTime = o1js_1.Experimental.ZkProgram({
         },
     },
 });
-function premiumTier() {
+function test() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const output = yield init();
+        console.log(output);
+    });
+}
+test();
+//////////////////////////////
+function main() {
     return __awaiter(this, void 0, void 0, function* () {
         console.log('o1js loaded');
         console.log('compiling...');
-        const { verificationKey } = yield ListeningTime.compile();
-        console.log('setting initial zero knowledge proof to 0');
-        const proof0 = yield ListeningTime.init((0, o1js_1.Field)(0));
+        const { verificationKey } = yield listeningTime.compile();
+        console.log('making proof 0');
+        const proof0 = yield listeningTime.init((0, o1js_1.Field)(0));
         console.log('making proof 1');
-        const proof1 = yield ListeningTime.addNumber((0, o1js_1.Field)(4), proof0, (0, o1js_1.Field)(4));
+        const proof1 = yield listeningTime.addNumber((0, o1js_1.Field)(4), proof0, (0, o1js_1.Field)(4));
         console.log('making proof 2');
-        const proof2 = yield ListeningTime.add((0, o1js_1.Field)(4), proof1, proof0);
+        const proof2 = yield listeningTime.add((0, o1js_1.Field)(4), proof1, proof0);
         console.log('verifying proof 2');
         console.log('proof 2 data', proof2.publicInput.toString());
         const ok = yield (0, o1js_1.verify)(proof2.toJSON(), verificationKey);
@@ -55,4 +77,4 @@ function premiumTier() {
         console.log('Shutting down');
     });
 }
-premiumTier();
+//  main();
--- code_radio/js/chapter_08/ts/zkprogram.ts
+++ code_radio/js/chapter_08/ts/zkprogram.ts
@@ -6,8 +6,26 @@ import {
   Experimental,
   verify,
 } from 'o1js';
+  
+let initialProof;
+let previousProof;
+let latestProof;
 
-const ListeningTime = Experimental.ZkProgram({
+let listeningGoal: number = 100;
+
+async function init() {
+    console.log('o1js loaded');
+
+    console.log('creating verification key...');
+    const { verificationKey } = await listeningTime.compile();
+  
+    console.log('creating initial listening time proof')
+    const initialProof = await listeningTime.init(Field(0));
+
+    return initialProof
+}
+
+const listeningTime = Experimental.ZkProgram({
     publicInput: Field,
   
     methods: {
@@ -44,29 +62,45 @@ const ListeningTime = Experimental.ZkProgram({
     },
   });
 
-async function premiumTier() {
-
-  console.log('o1js loaded');
-
-  console.log('compiling...');
-  const { verificationKey } = await ListeningTime.compile();
-
-  console.log('setting initial zero knowledge proof to 0')
-  const proof0 = await ListeningTime.init(Field(0));
-
-  console.log('making proof 1')
-  const proof1 = await ListeningTime.addNumber(Field(4), proof0, Field(4));
-
-  console.log('making proof 2')
-  const proof2 = await ListeningTime.add(Field(4), proof1, proof0);
-
-  console.log('verifying proof 2');
-  console.log('proof 2 data', proof2.publicInput.toString());
-
-  const ok = await verify(proof2.toJSON(), verificationKey);
-  console.log('ok', ok);
-
-  console.log('Shutting down');
+async function test() {
+    const output = await init();
+    console.log(output);
 }
 
-premiumTier();
+test();
+
+//////////////////////////////
+
+async function main() {
+  
+    console.log('o1js loaded');
+  
+    console.log('compiling...');
+  
+    const { verificationKey } = await listeningTime.compile();
+  
+    console.log('making proof 0')
+  
+    const proof0 = await listeningTime.init(Field(0));
+  
+    console.log('making proof 1')
+  
+    const proof1 = await listeningTime.addNumber(Field(4), proof0, Field(4));
+  
+    console.log('making proof 2')
+  
+    const proof2 = await listeningTime.add(Field(4), proof1, proof0);
+  
+    console.log('verifying proof 2');
+    console.log('proof 2 data', proof2.publicInput.toString());
+  
+    const ok = await verify(proof2.toJSON(), verificationKey);
+    console.log('ok', ok);
+  
+    console.log('Shutting down');
+  
+  }
+
+//  main();
+
+
